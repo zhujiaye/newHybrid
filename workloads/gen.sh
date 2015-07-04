9a859df5af3ecf7788b841ff2733786e8bef8c20
zhujiaye <zhujiaye1@gmail.com>
2015-07-04 12:56:30 +0800

    generate workloads

--- workloads/gen.sh
+++ workloads/gen.sh
@@ -1,10 +1,12 @@
 for ((i=1;i<=5;i++))
 do
-	java -jar WorkloadGenerator.jar
-	mv load.txt load550500_0.20.1_test$i.txt
 	java -jar WorkloadGenerator.jar 0.25
 	mv load.txt load550500_0.250.1_test$i.txt
 	java -jar WorkloadGenerator.jar 0.3
 	mv load.txt load550500_0.30.1_test$i.txt
+	java -jar WorkloadGenerator.jar 0.35
+	mv load.txt load550500_0.350.1_test$i.txt
+	java -jar WorkloadGenerator.jar 0.4
+	mv load.txt load550500_0.40.1_test$i.txt
 	
 done
